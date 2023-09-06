58ad2ece88a0e243885d0156d50672e3190131fb
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-06 12:17:45 +0200

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,20 +1,45 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Typewriter from 'typewriter-effect';
 // import { IoClose } from 'react-icons/io'
-import { AiOutlineCloseCircle } from 'react-icons/ai'
+// import { AiOutlineCloseCircle } from 'react-icons/ai'
 
 function Modal() {
+    // const [showCloseIcon, setShowCloseIcon] = useState(false)
     const dialogRef = useRef(null)
 
     useEffect(() => {
-        setTimeout(() => {
-            dialogRef.current.showModal()
-        }, 1000)
+        dialogRef.current.showModal()
+        // setTimeout(() => {
+        //     dialogRef.current.showModal()
+        // }, 1000)
 
         setTimeout(() => {
             dialogRef.current.close()
         }, 17000)
     }, [])
+
+    // function closeModal() {
+    //     console.log('clicked')
+    //     dialogRef.current.close()
+    // }
+    
+    function closeModalonClickOutside(e) {
+        const dialogDimensions = dialogRef.current.getBoundingClientRect()
+        if (
+            e.clientX < dialogDimensions.left ||
+            e.clientX > dialogDimensions.right ||
+            e.clientY < dialogDimensions.top ||
+            e.clientY > dialogDimensions.bottom
+        ) {
+            dialogRef.current.close()
+        }
+    }
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setShowCloseIcon(true)
+    //     }, 100);
+    // }, [])
 
     const textArray = [
         "New to this app?",
@@ -25,28 +50,28 @@ function Modal() {
     ]
 
     return (
-        <dialog ref={dialogRef} className='modal'>
-            {setTimeout(() => {
-                <AiOutlineCloseCircle />
-            }, 1000)}
+        <dialog ref={dialogRef} onClick={closeModalonClickOutside} className='modal'>
             <Typewriter
                 onInit={(typewriter) => {
-                    typewriter.pauseFor(1000)
-                        .changeDelay(50)
-                        .typeString(`<p>${textArray[0]}</p>`)
-                        .pauseFor(1000)
-                        .deleteAll(`<p>${textArray[0]}</p>`)
-                        .typeString(`<p>${textArray[1]}</p>`)
-                        .pauseFor(1000)
-                        .deleteAll(`<p>${textArray[1]}</p>`)
-                        .typeString(`<p>${textArray[2]}</p>`)
-                        .pauseFor(1500)
-                        .typeString(`<p>${textArray[3]}</p>`)
-                        .pauseFor(1500)
-                        .typeString(`<p>${textArray[4]}</p>`)
-                        .start();
+                    typewriter
+                    // .pauseFor(1000)
+                    .changeDelay(50)
+                    .typeString(`<p>${textArray[0]}</p>`)
+                    .pauseFor(1000)
+                    .deleteAll(`<p>${textArray[0]}</p>`)
+                    .typeString(`<p>${textArray[1]}</p>`)
+                    .pauseFor(1000)
+                    .deleteAll(`<p>${textArray[1]}</p>`)
+                    .typeString(`<p>${textArray[2]}</p>`)
+                    .pauseFor(1500)
+                    .typeString(`<p>${textArray[3]}</p>`)
+                    .pauseFor(1500)
+                    .typeString(`<p>${textArray[4]}</p>`)
+                    .start();
                 }}
-            />
+                />
+            {/* {showCloseIcon && <AiOutlineCloseCircle className='close-icon'/>} */}
+            {/* <button onClick={closeModal} className='btn close-btn'>Close</button> */}
         </dialog>
     );
 }
