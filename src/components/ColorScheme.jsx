c34d4abfaa3e1ef900446af250a3e8c0db61ea2a
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-06 16:16:55 +0200

    Update ColorScheme.jsx

--- src/components/ColorScheme.jsx
+++ src/components/ColorScheme.jsx
@@ -3,11 +3,28 @@ import { AppContext } from '../context/AppContext'
 import Color from "./Color"
 
 function ColorScheme() {
-    const { userSelection } = useContext(AppContext)
+    const { userSelection, copyHexCode, copiedHexCode } = useContext(AppContext)
 
     return (
         <section className="color-scheme-container">
-            <p className='p--large'>Selected seed color: <span className='border-bottom'>{userSelection.seed.toUpperCase()}</span></p>
+            <div className='color-scheme-container__top'>
+                <p className='p--large'>Selected seed color:</p>
+                <p 
+                    onClick={() => copyHexCode(userSelection.seed)}
+                    className='p--large p--seed-color'
+                >
+                    {copiedHexCode === userSelection.seed ? 'Copied!' : userSelection.seed.toUpperCase()}
+                </p>
+            </div>
+            {/* <p className='p--large'>
+                Selected seed color: 
+                <span
+                    onClick={() => copyHexCode(userSelection.seed)} 
+                    className='p--seed-color'
+                >
+                    {copiedHexCode === userSelection.seed ? 'Copied!' : userSelection.seed.toUpperCase()}
+                </span>
+            </p> */}
             <div className='scheme-wrapper'>
                 <Color />
             </div>
