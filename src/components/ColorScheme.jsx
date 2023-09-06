fbfcd3b48b562e1f5e8ba39718c37a0d18b90144
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-06 20:00:42 +0200

    Update ColorScheme.jsx

--- src/components/ColorScheme.jsx
+++ src/components/ColorScheme.jsx
@@ -1,19 +1,23 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import Color from "./Color"
 
 function ColorScheme() {
     const { userSelection, copyHexCode, copiedHexCode } = useContext(AppContext)
 
+    const [text, setText] = useState(userSelection.seed)
+
     return (
         <section className="color-scheme-container">
             <div className='color-scheme-container__top'>
-                <p className='p--large'>Selected seed color:</p>
+                <p className='p--large top--left'>Selected seed color:</p>
                 <p 
                     onClick={() => copyHexCode(userSelection.seed)}
-                    className='p--large p--seed-color'
+                    onMouseEnter={() => setText('Copy hex code')}
+                    onMouseLeave={() => setText(userSelection.seed.toUpperCase())}
+                    className='p--large top--right'
                 >
-                    {copiedHexCode === userSelection.seed ? 'Copied!' : userSelection.seed.toUpperCase()}
+                    {copiedHexCode === userSelection.seed ? 'Copied!' : text}
                 </p>
             </div>
             {/* <p className='p--large'>
