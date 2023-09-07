c59fe6ab16d4338d30baef0d0e6c9653ea64fdc3
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-07 12:54:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import Footer from './components/Footer'
 import Modal from './components/Modal'
 
 function App() {
-  const [isNewUser, setIsNewUser] = useState(true)
+  const [isNewUser, setIsNewUser] = useState(false)
 
 //--- Check if it is a new user or not. If new user, the modal will be rendered, if not the modal will not be rendered --///
   useEffect(() => {
-    // const isUserInLocalStorage = localStorage.getItem('isNewUser')
-    // !isUserInLocalStorage && setIsNewUser(true)
-    // localStorage.setItem('isNewUser', 'false')
+    const isUserInLocalStorage = localStorage.getItem('isNewUser')
+    !isUserInLocalStorage && setIsNewUser(true)
+    localStorage.setItem('isNewUser', 'false')
   }, [])
 
   return (
