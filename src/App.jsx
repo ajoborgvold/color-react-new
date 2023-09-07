ff0b06149659fe8d3d53ba2e660cc843dc4d44c6
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-07 12:12:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
-import { useContext } from 'react'
-import { AppContext } from './context/AppContext'
+import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Form from './components/Form'
 import ColorScheme from './components/ColorScheme'
@@ -8,13 +7,22 @@ import Footer from './components/Footer'
 import Modal from './components/Modal'
 
 function App() {
+  const [isNewUser, setIsNewUser] = useState(false)
+
+//--- Check if it is a new user or not. If new user, the modal will be rendered, if not the modal will not be rendered --///
+  useEffect(() => {
+    const isUserInLocalStorage = localStorage.getItem('isNewUser')
+    !isUserInLocalStorage && setIsNewUser(true)
+    localStorage.setItem('isNewUser', 'false')
+  }, [])
+
   return (
     <div className='app-container'>
       <Header />
       <Form />
       <ColorScheme />
       <Footer />
-      <Modal />
+      {isNewUser && <Modal />}
     </div>
   )
 }
