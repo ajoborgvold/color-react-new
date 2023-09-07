3c70b95c3ef01a7d402fddc503cf63b412e098ff
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-07 18:00:16 +0200

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,7 +10,7 @@ function Modal() {
 
         setTimeout(() => {
             dialogRef.current.close()
-        }, 18000)
+        }, 19000)
     }, [])
     
 //--- Close the modal by clicken anywhere outside of it ---//
