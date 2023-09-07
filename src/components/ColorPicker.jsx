b19e6869b6f71b4d3282f7234f628412de65357c
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-07 11:24:19 +0200

    Update ColorPicker.jsx

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -6,28 +6,52 @@ import useClickOutside from "../utils/useClickOutside";
 function ColorPicker() {
     const [isOpen, setIsOpen] = useState(false)
     const popover = useRef()
-    const { handleColorChange, userSelection } = useContext(AppContext)
+    const swatch = useRef()
+    const { handleColorChange, userSelection } = useContext(AppContext)      
 
     const close = useCallback(() => setIsOpen(false), [])
+
     useClickOutside(popover, close)
+
+    function handleKeyDownOnSwatch(e) {
+        console.log(e.key)
+        if (e.key === 'Enter') {
+            setIsOpen(true)
+        } else if (e.key === 'Backspace') {
+            setIsOpen(false)
+        }
+    }
+
+    function handleKeyDownOnPopover(e) {
+        if (e.key === 'Backspace') {
+            setIsOpen(false)
+            swatch.current.focus()
+        }
+    }
 
     return (
         <div className="color-picker-wrapper">
             <div
+                ref={swatch}
                 tabIndex={0}
                 onClick={() => setIsOpen(true)}
-                // onKeyDown={() => setIsOpen(true)}
+                onKeyDown={handleKeyDownOnSwatch}
                 style={{
                     backgroundColor: userSelection.seed
                 }}
                 className="swatch"
-            >
+                >
             </div>
             {isOpen && (
-                <div ref={popover} className="popover">
+                <div 
+                    ref={popover}
+                    onKeyDown={handleKeyDownOnPopover}
+                    className="popover"
+                >
                     <label htmlFor="color-picker" className="label">Select a seed color</label>
                     <HexColorPicker
                         onChange={handleColorChange}
+                        onFocus={() => setIsOpen(true)}
                         color={userSelection.seed}
                         id="color-picker"
                     />
