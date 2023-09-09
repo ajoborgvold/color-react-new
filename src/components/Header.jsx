2bae5ebc40829674f4695e40ad2adb0b53e6ffb1
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-09 16:31:12 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,54 +1,61 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
+import { AppContext } from "../context/AppContext"
+
 
 function Header() {
     const [theme, setTheme] = useState('light')
+    const { setShowColorPicker } = useContext(AppContext)
 
-    // Handle the user's theme preferences
-        useEffect(() => {
-            const userThemePref = getUserThemePref()
-            const mediaQueryPref = getMediaQueryPref()
-    
-            if (userThemePref) {
-                setTheme(userThemePref)
-            } else {
-                setTheme(mediaQueryPref)
-            }
-    
-            document.body.dataset.theme = theme
-    
-        }, [theme])
-        
-    // Toggle theme in the app UI
-        function toggleTheme() {
-            const newTheme = theme === 'dark' ? 'light' : 'dark'
-            setTheme(newTheme)
-            storeUserThemePref(newTheme)
-            document.body.dataset.theme = theme
+// Handle the user's theme preferences
+    useEffect(() => {
+        const userThemePref = getUserThemePref()
+        const mediaQueryPref = getMediaQueryPref()
+
+        if (userThemePref) {
+            setTheme(userThemePref)
+        } else {
+            setTheme(mediaQueryPref)
         }
-    
+
+        document.body.dataset.theme = theme
+
+    }, [theme])
+
+// Get the user's theme preference from their OS
+    function getMediaQueryPref() {
+        const mediaQuery = "(prefers-color-scheme: dark)"
+        const mql = window.matchMedia(mediaQuery)
+        const hasPreference = typeof mql.matches === "boolean"
+
+        if (hasPreference) {
+            return mql.matches ? "dark" : "light";
+        }
+    }
+
+// Toggle theme in the app UI
+    function toggleTheme() {
+        const newTheme = theme === 'dark' ? 'light' : 'dark'
+        setTheme(newTheme)
+        storeUserThemePref(newTheme)
+        document.body.dataset.theme = theme
+    }
+
     // Store the user's theme preference in local storage
-        function storeUserThemePref(pref) {
-            localStorage.setItem('theme', pref)
-        }
-    
-        function getUserThemePref() {
-            return localStorage.getItem('theme')
-        }
-    
-    // Get the user's theme preference from their OS
-        function getMediaQueryPref() {
-            const mediaQuery = "(prefers-color-scheme: dark)"
-            const mql = window.matchMedia(mediaQuery)
-            const hasPreference = typeof mql.matches === "boolean"
-            
-            if (hasPreference) {
-                return mql.matches ? "dark" : "light";
-            }
-        }
+    function storeUserThemePref(pref) {
+        localStorage.setItem('theme', pref)
+    }
 
+    function getUserThemePref() {
+        return localStorage.getItem('theme')
+    }
+    
     return (
         <header className="header">
-            <button onClick={toggleTheme} className="btn theme-btn">
+            <button 
+                onClick={toggleTheme} 
+                onFocus={() => setShowColorPicker(false)} 
+                className="btn theme-btn"
+            >
                 {theme === 'light' ? 'Dark' : 'Light'}
             </button>
         </header>
