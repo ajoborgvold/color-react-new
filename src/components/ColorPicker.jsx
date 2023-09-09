2a20b38dc27d077d9f8d0320e83e883f206b2ca4
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-09 16:30:58 +0200

    Update ColorPicker.jsx

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,58 +1,41 @@
-import { useContext, useState, useRef, useCallback } from "react";
+import { useContext, useRef, useCallback } from "react";
 import { HexColorPicker } from "react-colorful";
 import { AppContext } from "../context/AppContext";
 import useClickOutside from "../utils/useClickOutside";
 
 function ColorPicker() {
-    const [isOpen, setIsOpen] = useState(false)
     const popoverRef = useRef()
-    const swatchRef = useRef()
-    const { handleSeedColorChange, userSelection } = useContext(AppContext)
+    const {
+        handleSeedColorChange,
+        userSelection,
+        showColorPicker,
+        setShowColorPicker
+    } = useContext(AppContext)
 
 //--- Close the popover when the user clicks outside the color picker ---//
-    const close = useCallback(() => setIsOpen(false), [])
-
+    const close = useCallback(() => setShowColorPicker(false), [])
     useClickOutside(popoverRef, close)
-
-//--- Handle keyboard navigation ---//    
-    function handleKeyDownOnSwatch(e) {
-        if (e.key === 'Enter') {
-            setIsOpen(true)
-        } else if (e.key === 'Backspace') {
-            setIsOpen(false)
-        }
-    }
-
-    function handleKeyDownOnPopover(e) {
-        if (e.key === 'Backspace') {
-            setIsOpen(false)
-            swatchRef.current.focus()
-        }
-    }
 
     return (
         <div className="color-picker-wrapper">
             <div
-                ref={swatchRef}
                 tabIndex={0}
-                onClick={() => setIsOpen(true)}
-                onKeyDown={handleKeyDownOnSwatch}
+                onClick={() => setShowColorPicker(!showColorPicker)}
+                onKeyDown={e => e.key === 'Enter' && setShowColorPicker(!showColorPicker)}
                 style={{
                     backgroundColor: userSelection.seed
                 }}
                 className="swatch"
                 >
             </div>
-            {isOpen && (
+            {showColorPicker && (
                 <div 
                     ref={popoverRef}
-                    onKeyDown={handleKeyDownOnPopover}
                     className="popover"
                 >
                     <label htmlFor="color-picker" className="label">Select a seed color</label>
                     <HexColorPicker
                         onChange={handleSeedColorChange}
-                        onFocus={() => setIsOpen(true)}
                         color={userSelection.seed}
                         id="color-picker"
                     />
