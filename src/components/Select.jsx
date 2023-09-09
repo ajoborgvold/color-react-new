d17866e149906426fda34e5093169878ec5dac77
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-09 16:31:24 +0200

    Update Select.jsx

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { AppContext } from "../context/AppContext"
 
 function Select() {
-    const { handleModeChange } = useContext(AppContext)
+    const { handleModeChange, setShowColorPicker } = useContext(AppContext)
 
     const optionsObj = {
         'monochrome': 'Monochrome',
@@ -24,7 +24,13 @@ function Select() {
     return (
         <div className="select-wrapper">
             <label htmlFor="mode" className="label">Select a color mode</label>
-            <select onChange={handleModeChange} name="mode" id="mode" className="select-element">
+            <select 
+                onChange={handleModeChange} 
+                onFocus={() => setShowColorPicker(false)} 
+                name="mode" 
+                id="mode" 
+                className="select-element"
+            >
                 {optionsEL}
             </select>
         </div>
