045150da74791f91cf52551dafa778a672948638
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-13 14:01:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import './App.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import Header from './components/Header'
 import Form from './components/Form'
 import ColorScheme from './components/ColorScheme'
 import Footer from './components/Footer'
 import Modal from './components/Modal'
+import { AppContext } from './context/AppContext'
 
 function App() {
   const [isNewUser, setIsNewUser] = useState(false)
+  const { setShowColorPicker } = useContext(AppContext)
 
 // Check if it is a new user or not. If new user, render the modal
   useEffect(() => {
     const isUserInLocalStorage = localStorage.getItem('isNewUser')
     !isUserInLocalStorage && setIsNewUser(true)
     localStorage.setItem('isNewUser', 'false')
+  }, [])
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.keyCode === 27) {
+        setShowColorPicker(false)
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
   }, [])
 
   return (
